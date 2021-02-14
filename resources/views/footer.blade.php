6e98e272024c2d1f2121e6f33f82c56aa3792d33
Tealea Erman <mouobouotealeaerman@gmail.com>
2021-02-14 07:07:44 +0100

    header footer and login done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="panel panel-default">
+    <div class="panel-body">
+        
+    </div>
+    <div class="panel-footer text-center">
+        © 2020 Copyright : Devesite
+    </div>
+</div>
